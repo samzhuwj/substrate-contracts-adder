cee80dc0f70d5eeee4da93f2cdb511ad4f098da6
samzhuwj <samzhuwj@gmail.com>
2018-11-14 22:36:52 +0800

    invoke wasm-build

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
 cargo +nightly build --release --target=wasm32-unknown-unknown --verbose
+wasm-build target substrate-contracts-adder --target-runtime=substrate --final=adder --save-raw=./target/adder-deployed.wasm --target wasm32-unknown-unknown
